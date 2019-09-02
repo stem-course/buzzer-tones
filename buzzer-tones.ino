076da8db8b0c00058de56d0dacd2129c66c01d67
munphurid <54735786+munphurid@users.noreply.github.com>
2019-09-02 17:39:48 +0500

    Code

---
+++ buzzer-tones.ino
@@ -0,0 +1,53 @@
+/*
+  Melody
+
+  Plays a melody
+
+  circuit:
+   8-ohm speaker on digital pin 8
+
+  created 21 Jan 2010
+  modified 30 Aug 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://arduino.cc/en/Tutorial/Tone
+
+*/
+int buzzer = 11;              //Buzzer is connected to pin11
+
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  2000, 2000, 2000, 2000, 2000  //Enter notes or frequencies here
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4      //Enter duration of each note or frequency
+};
+
+void setup() {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(buzzer, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(buzzer);
+  }
+}
+
+void loop() {
+  // no need to repeat the melody.
+}
